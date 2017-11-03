7ca2e7e6ef449c968925268972accb41889e0b31
Diphoko <SelloJacob.Diphoko@standardbank.co.za>
2017-11-03 02:03:07 +0200

    C#3

--- plainProject/src/plainProject/plainProjJav.java
+++ plainProject/src/plainProject/plainProjJav.java
@@ -3,7 +3,7 @@ package plainProject;
 public class plainProjJav {
 
 	public static void main(String[] args) {
-		// Commentang mooo
+		// Commentang mooo zooo
 		System.out.println("");
 		System.out.println("Plain Project :-)");
 		System.out.println("");
